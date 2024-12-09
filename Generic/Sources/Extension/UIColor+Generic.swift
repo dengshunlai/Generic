9fe46e4a72d0e1537e5d17820af2439da07d81e2
邓顺来 <mu3305495@163.com>
2024-12-09 21:20:10 +0800

    code

--- Generic/Sources/Extension/UIColor+Generic.swift
+++ Generic/Sources/Extension/UIColor+Generic.swift
@@ -34,6 +34,21 @@ public extension GenericNameSpace where T == UIColor {
         return UIColor.init(red: red, green: green, blue: blue, alpha: 1)
     }
     
+    static func hexColor(_ hex: UInt32) -> UIColor {
+        let color: UInt32 = hex
+        
+        let mask = 0x000000FF
+        let r = Int(color >> 16) & mask
+        let g = Int(color >> 8) & mask
+        let b = Int(color) & mask
+        
+        let red   = CGFloat(r) / 255.0
+        let green = CGFloat(g) / 255.0
+        let blue  = CGFloat(b) / 255.0
+        
+        return UIColor.init(red: red, green: green, blue: blue, alpha: 1)
+    }
+    
     // UIColor -> Hex String
     func hexString() -> String? {
         var red: CGFloat = 0
