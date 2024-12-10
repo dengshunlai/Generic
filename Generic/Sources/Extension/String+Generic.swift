6724dd771a66f31e2dd25f75d162fd9ecef1504a
邓顺来 <mu3305495@163.com>
2024-12-10 11:37:15 +0800

    code

---
+++ Generic/Sources/Extension/String+Generic.swift
@@ -0,0 +1,51 @@
+//
+//  String+SubString.swift
+//  Generic
+//
+//  Created by 邓顺来1992 on 2024/12/10.
+//
+
+import Foundation
+
+///这里没用Generic名称空间，因为感觉写起来麻烦，
+///但是直接这样定义的话有可能会跟其他模块冲突
+
+///获取子字符串："Hello, world!"[0...5]
+extension String {
+    subscript (i: Int) -> Character {
+        return self[index(startIndex, offsetBy: i)]
+    }
+    
+    subscript (bounds: CountableRange<Int>) -> Substring {
+        let start = index(startIndex, offsetBy: bounds.lowerBound)
+        let end = index(startIndex, offsetBy: bounds.upperBound)
+        if end < start { return "" }
+        return self[start..<end]
+    }
+    
+    subscript (bounds: CountableClosedRange<Int>) -> Substring {
+        let start = index(startIndex, offsetBy: bounds.lowerBound)
+        let end = index(startIndex, offsetBy: bounds.upperBound)
+        if end < start { return "" }
+        return self[start...end]
+    }
+    
+    subscript (bounds: CountablePartialRangeFrom<Int>) -> Substring {
+        let start = index(startIndex, offsetBy: bounds.lowerBound)
+        let end = index(endIndex, offsetBy: -1)
+        if end < start { return "" }
+        return self[start...end]
+    }
+    
+    subscript (bounds: PartialRangeThrough<Int>) -> Substring {
+        let end = index(startIndex, offsetBy: bounds.upperBound)
+        if end < startIndex { return "" }
+        return self[startIndex...end]
+    }
+    
+    subscript (bounds: PartialRangeUpTo<Int>) -> Substring {
+        let end = index(startIndex, offsetBy: bounds.upperBound)
+        if end < startIndex { return "" }
+        return self[startIndex..<end]
+    }
+}
